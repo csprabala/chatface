244442acd4cd8a97b8a2ee8ff49506fdf24c817e
Chandrasekhar Prabala <chandrasekhar85@hotmail.com>
2023-11-11 15:39:07 +0000

    Hello World Component

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+type Props = {
+  name: string;
+};
+
+const HelloWorld: React.FC<Props> = ({ name }) => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+export default HelloWorld;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
